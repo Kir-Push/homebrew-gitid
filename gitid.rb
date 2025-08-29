bafb912479d6cc809a5949e15bdb6b35f8900eb1
Kirill <Ashparenij2@gmail.com>
2025-08-30 01:42:01 +0300

    Add formula for gitid v0.1.0

---
+++ gitid.rb
@@ -0,0 +1,19 @@
+    # Formula for installing the GitID CLI tool
+    class Gitid < Formula
+      desc "Git Identity Manager to easily switch between Git identities"
+      homepage "https://github.com/Kir-Push/GitID"
+      url "https://github.com/Kir-Push/GitID/archive/refs/tags/v0.1.0.tar.gz" 
+      sha256 "ba3c027849c43b7fb62fa0a124718593a274391a6428843d7585ab0612283fab"
+      license "MIT" # Or the license you are using
+
+      depends_on "go" => :build
+
+      def install
+        system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/gitid"
+      end
+
+      test do
+        # This is a basic test to ensure the binary was installed correctly.
+        assert_match "GitID is a lightweight CLI tool", shell_output("#{bin}/gitid --help")
+      end
+    end
